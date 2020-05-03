e5039d2fe59602955e04e58e09836b361fdbff74
julianssb <52377477+julianssb@users.noreply.github.com>
2020-05-03 15:14:58 +0200

    Update veprime.php

--- loginsystem/veprime.php
+++ loginsystem/veprime.php
@@ -144,5 +144,41 @@ function checkLogin(){
   }
   }
 
+//forgot password
+if (isset($_POST['reset_password'])) {
+  pass_reset();
+}
+
+function pass_reset(){
+  $email = mysqli_real_escape_string($GLOBALS['connection'], $_POST['email']);
+
+  $sql = "SELECT email FROM users WHERE email = '$email'";
+  $conn = mysqli_query($GLOBALS['connection'], $sql);
+  $count = mysqli_num_rows($conn);
+
+  if ($count == 1) {
+    $row = mysqli_fetch_assoc($conn);
+    $to = $row['email'];
+
+    //krijojme nje string random dhe ja cojme userit ne email
+    $pass= rand(999, 99999);
+    $password_hash = md5($pass);
+
+    $subject = "Your recovered password";
+    $message = "use this password to login " . $password_hash;
+
+  
+    if (mail($to, $subject, $message)) {
+      echo "Your password was sent to your email";
+    }else {
+      echo "Try again";
+    }
+
+    //ndryshojme pass ne db
+    $u_sql = "UPDATE users SET password = '$password_hash' WHERE email = '$email' ";
+    $res = mysqli_query($GLOBALS['connection'], $u_sql);
+  }
+}
+
 
 ?>
