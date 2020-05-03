13303ada866ecf26a83f94982bdc4f9f8dbe830f
FrensisXh <64225631+FrensisXh@users.noreply.github.com>
2020-05-03 19:31:00 +0200

    Add files via upload

--- loginsystem/enter_email.php
+++ loginsystem/enter_email.php
@@ -7,19 +7,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Password Reset PHP</title>
-	<link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" href="enter_email.css">
 </head>
 <body>
-	<form method="post">
+  <form method="post" id="reset-box">
+	<div class="div-box"> 
+	  <img src="logo1.png" alt="Wander around Tirana" class="img">
 		<h2 class="form-title">Reset password</h2>
-
 		<div class="form-group">
-			<label>Your email address</label>
-			<input type="email" name="email">
-		</div>
-		<div class="form-group">
-			<button type="submit" name="reset_password">Submit</button>
-		</div>
+			<input type="email" placeholder="Type your e-mail address" name="email">
+		</div> <br> <br>
+		 <button class="btn" type="submit" name="reset_password" >Submit</button>
+		
+    </div>
 	</form>
 </body>
 </html>
