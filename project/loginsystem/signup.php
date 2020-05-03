fa721bfdca085365e917d45d849204293f974c92
julianssb <52377477+julianssb@users.noreply.github.com>
2020-05-04 00:34:53 +0200

    Update signup.php

--- project/loginsystem/signup.php
+++ project/loginsystem/signup.php
@@ -1,30 +1,35 @@
 <?php
 require_once("veprime.php");
+
 ?>
-<html>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
     <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Comptatible" content="ie=edge"> <!-- perdoret per te lejuar perdorues te zgjedhin ca versioni i Internet Explorer do paraqitet -->
+      <link rel="stylesheet" href="signup.css">
         <title> Sign up </title>
-        <link rel="stylesheet" href="signup.css" type="text/css" media="all" />
 </head>
 
-<body>
-
+<body style="background-image: url('s7-4.jpg')">
 
 <form method="post" id="box-signup">
   <div class="signup">
    <div class="djathtas">
-    <img src="foto_login_signup/logo1.png" alt="Wander around Tirana" style="width:80px;height:75px" class="logo">
+    <img src="logo1.png" alt="Wander around Tirana" style="width:80px;height:75px" class="logo">
       <p class="sign-up">Create an account</p>
-      <div class="emer">
-      <input type="text" placeholder="First name" name="first_name">
-       </div>
-       <br>
-     <div class="mbiemer">
-    <input type="text" placeholder="Last name" name="last_name">
-    </div>
+        <div class="emer">
+        <input type="text" placeholder="First name" name="first_name" >
+         </div>
+         <br>
+       <div class="mbiemer">
+      <input type="text" placeholder="Last name" name="last_name">
+        </div>
          <br>
         <div class="email">
-          <input type="text" placeholder="E-mail" name="email" value="">
+          <input type="text" placeholder="E-mail" name="email">
         </div>
          <br>
         <div class="email">
@@ -34,7 +39,6 @@ require_once("veprime.php");
         <div class="email">
           <input type="password" placeholder="Confirm password" name="confirmPassword" >
         </div>
-        <br>
 
         <?php
         if (isset($_SESSION['error'])) {
@@ -43,11 +47,13 @@ require_once("veprime.php");
         }
          ?>
 
+        <br>
         <input class="btn" type="submit" name="SignUp" value="Sign Up">
          <p style="text-align: center; color: black; font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
                 font-size:14px;padding-top: -50px; width:50%; display: table-cell; padding-left: 100px;
                 padding-right: 5px;padding-bottom: 30px;"> Already a member?</p>
-         <p class="member"><a href="login.php"> Login <a> </p>
+         <p class="member"><a href="login.php"> Login </a></p>
+
         </div>
         <div class="majtas">    </div>
       </div>
