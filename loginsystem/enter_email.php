df7aaf5fa623cbfbb448278e179f5133a3d781e3
julianssb <52377477+julianssb@users.noreply.github.com>
2020-05-03 15:15:40 +0200

    forgot password

---
+++ loginsystem/enter_email.php
@@ -0,0 +1,25 @@
+<?php
+  require_once("veprime1.php");
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Password Reset PHP</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<form method="post">
+		<h2 class="form-title">Reset password</h2>
+
+		<div class="form-group">
+			<label>Your email address</label>
+			<input type="email" name="email">
+		</div>
+		<div class="form-group">
+			<button type="submit" name="reset_password">Submit</button>
+		</div>
+	</form>
+</body>
+</html>
