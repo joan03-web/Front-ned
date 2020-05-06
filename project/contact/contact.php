680ad28900bcdb1e8b8449331c8c5d265be61d89
joan03-web <64017409+joan03-web@users.noreply.github.com>
2020-05-06 17:22:14 +0200

    Update contact.php

--- project/contact/contact.php
+++ project/contact/contact.php
@@ -1,40 +1,96 @@
 <?php
-  require_once("veprime.php");
+  require_once("v_contact.php");
   ?>
 
-<html>
+<html lang="en">
 <head>
-<link rel="stylesheet" href="style.css" type="text/css" media="all" />
-</head>
-<body>
-  
-<h2>Contact Us</h2>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=decive-width, initial-scale=1.0">
+  <meta http-http-equiv="X-UA-Compatible" content="ie-edge">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+  rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700"
+   rel="stylesheet">
 
-  <form class="form" action="fq_contact.php" method="POST">
 
-    <p class="username">
-      <input type="text" name="name" id="name" placeholder="Enter your name" >
+  <link rel="stylesheet" href="contact.css">
+  </head>
 
-    </p>
+  <body>
+    <div class="wrapper">
+      <div class = "homepage">
+            <img class="logo" src="../loginsystem/foto_login_signup/logo transparente.png" alt="Wander Around Tirana">
+        <p class="Wat"> Wander Around Tirana</p>
+        <p class="Wat2">In Tirana there is always more than what meets the eye</p>
+        <div class="list">
+            <ul>
+                <li> <a href="/project/loginsystem/Homepage.php">
+                    <div class="icon">
+                        <i class="fa fa-home"></i>
+                        <i class="fa fa-home"></i>
+                    </div>
+                    Home </a>
+                </li>
+                <li> <a href="#">
+                    <div class="icon">
+                        <i class="fa fa-users"></i>
+                        <i class="fa fa-users"></i>
+                    </div>
+                    About us </a>
+                </li>
+                <li> <a href="#">
+                    <div class="icon">
+                        <i class="fa fa-map-marker"></i>
+                        <i class="fa fa-map-marker"></i>
+                    </div>
+                    Explore Tirana </a>
+                </li>
+                <li> <a href="contact.php">
+                    <div class="icon">
+                        <i class="fa fa-envelope"></i>
+                        <i class="fa fa-envelope"></i>
+                    </div>
+                    Contact us</a>
+                </li>
+                <li> <a href="#">
+                    <div class="icon">
+                        <i class="fa fa-user"></i>
+                        <i class="fa fa-user"></i>
+                    </div>
+                    Profile </a>
+                 </li>
+                <li> <a href="#">
+                    <div class="icon">
+                        <i class="fa fa-user-plus"></i>
+                        <i class="fa fa-user-plus"></i>
+                    </div>
+                    Be a member </a>
+                    <ul>
+                        <li> <a href="/project/loginsystem/signup.php"> Sign Up </a></li>
+                        <li> <a href="/project/loginsystem/login.php"> Login </a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
 
-    <p class="useremail">
-      <input type="text" name="email" id="email" placeholder="mail@example.com" >
-    </p>
+         <div class="header">
+        <h1>Let us help you</h1>
+      </div>
+      <div class="forma">
+        <form class="form" action="contact.php" method="POST">
+          <input type="text" name="name" placeholder="Name" required/>
+            <input type="text" name="email" placeholder="E-mail" required/>
+            <textarea id="msg" rows="6" cols="100" name="comment">Message</textarea>
+            <?php
+            if (isset($_SESSION['error'])) {
+              echo '<p style="color:red">'. htmlentities($_SESSION['error'])."</p>\n";
+              unset($_SESSION['error']);//e fshim
+            }
+             ?>
+             <button type="submit" name="submit"class="button">SEND</button>
 
-    <p class="usertext">
-      <textarea name="text" placeholder="Write something to us" ></textarea>
-    </p>
-
-    <?php
-    if (isset($_SESSION['error'])) {
-      echo '<p style="color:red">'. htmlentities($_SESSION['error'])."</p>\n";
-      unset($_SESSION['error']);//e fshim
-    }
-     ?>
-
-    <p class="usersubmit">
-      <input type="submit" name="submit" value="Send" >
-    </p>
-  </form>
+        </form>
+     </div>
+    </div>
 </body>
 </html>
