c44d6eed6c3a22898af15aac7259e7ce31aea84c
joan03-web <64017409+joan03-web@users.noreply.github.com>
2020-05-06 17:24:18 +0200

    Update veprime.php

--- project/contact_us/veprime.php
+++ project/contact_us/veprime.php
@@ -5,29 +5,27 @@
   session_start();
 
   $connection = CreateDatabase();
-  //check whether submit button is pressed or not
+
 if((isset($_POST['submit']))){
   createData();
 }
 
 function createData(){
-  //fetching and storing the form data in variables
 
   $Name = mysqli_real_escape_string($GLOBALS['connection'], $_POST['name']);
   $Email = mysqli_real_escape_string($GLOBALS['connection'], $_POST['email']);
-  $comments = mysqli_real_escape_string($GLOBALS['connection'], $_POST['text']);
+  $comments = mysqli_real_escape_string($GLOBALS['connection'], $_POST['comment']);
     //query to insert the variable data into the database
     if(empty($Name) || empty($Email) || empty($comments)){
       $_SESSION['error'] = "Complete the form";
-      header("Location: fq_contact.php");
+      header("Location: contact.php");
       exit();
     }
 
   $sql="INSERT INTO contacte (name, email, comments) VALUES ('$Name','$Email', '$comments')";
-    //Execute the query and returning a message
     mysqli_query($GLOBALS['connection'], $sql);
-        header("Location: fq_contact.php");
-        echo '<script>alert("Welcome to Geeks for Geeks")</script>';
+    $_SESSION['error'] = "Mesage sent, thank you for contacting us!";
+        header("Location: contact.php");
         exit();
   }
 
